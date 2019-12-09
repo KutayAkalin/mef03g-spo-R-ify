252595800bdde94ab1f21eb86835334a65d5d149
Dost Karaahmetli <54936227+karaahmetlid@users.noreply.github.com>
2019-12-09 13:35:33 +0300

    Add files via upload

---
+++ SpotifyR/appShHor.R
@@ -0,0 +1,91 @@
+library(spotifyr)
+library(shiny)
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(reprex)
+library(tidyr)
+library(scales)
+library(DT)
+library(plotly)
+library(treemap)
+library(knitr)
+
+
+ui <- fluidPage(
+  titlePanel("Musical Horoscpoe"),
+  textInput("userName", "Please Enter Your Username", value = "spotifycharts"),
+  textInput("userPL", "Please Enter Your Playlist Code", value = "7mJKc32vPRWxI8dg8awSus"),
+  tabsetPanel(
+    tabPanel(title = " Playlist Key Attributes", plotOutput("userPlot", width = 700, height = 700)),
+    tabPanel(title = " Playlist Key Characteristics", plotOutput("userTree", width = 900, height = 600)),
+    tabPanel(title = " Personality Type", h3("What is your personality type?"), br(),  
+p("The key signatures of music are kind of like the signs of the zodiac."),  
+"If you take all the music in a given key, you’ll discover certain characteristics — 
+characteristics that can define your personality.", hr(), tableOutput("userPersona"), hr(), br(),
+print(em("The information above is for display purposes only and does not depend on any scientific, 
+astrological or musical know-how.")))
+  )
+)
+
+
+
+server <- function(input, output) {
+  Sys.setenv(SPOTIFY_CLIENT_ID = 'f5adea41ba0c4184a3d15e9960b4a0c2')
+  Sys.setenv(SPOTIFY_CLIENT_SECRET = '3b5362e9b6a44ea6a774911ae7700334')
+  access_token <- get_spotify_access_token()
+  
+  output$userPlot <- renderPlot({
+    uSer <- get_playlist_audio_features(input$userName, input$userPL) %>%
+      select(danceability, energy, speechiness, instrumentalness, liveness, valence, acousticness) %>%
+      sapply(FUN = mean) %>% as.data.frame()
+    
+    ggplot(uSer, aes(x = rownames(uSer), y = ., fill = rownames(uSer))) + 
+      geom_col() + 
+      coord_polar()
+  })
+  output$userTree <- renderPlot({
+    get_playlist_audio_features(input$userName, input$userPL) %>% group_by(key_name) %>%
+      summarise(count = n()) %>%
+      treemap(index = "key_name", vSize = "count", type = "index", 
+              palette = "Set1", 
+              title = paste(toString(input$userName), " Playlist Key Characteristics"), 
+              fontsize.title=16) 
+  })
+  output$userPersona <- renderTable ({
+    personaKey <- get_playlist_audio_features(input$userName, input$userPL) %>% group_by(key_name) %>%
+      summarise(count = n()) %>% filter(count == max(count))
+    if (personaKey$key_name == "A") {
+      personaInfo <- read.csv2("https://github.com/pjournal/mef03g-spo-R-ify/blob/master/SpotifyR/A.csv")
+    } else if (personaKey$key_name == "A#") {
+      personaInfo <- read.csv2("https://github.com/pjournal/mef03g-spo-R-ify/blob/master/SpotifyR/Amj.csv")
+    } else if (personaKey$key_name == "B") {
+      personaInfo <- read.csv2("https://github.com/pjournal/mef03g-spo-R-ify/blob/master/SpotifyR/B.csv")
+    } else if (personaKey$key_name == "C") {
+      personaInfo <- read.csv2("https://github.com/pjournal/mef03g-spo-R-ify/blob/master/SpotifyR/C.csv")
+    } else if (personaKey$key_name == "C#") {
+      personaInfo <- read.csv2("https://github.com/pjournal/mef03g-spo-R-ify/blob/master/SpotifyR/Cmj.csv")
+    } else if (personaKey$key_name == "D") {
+      personaInfo <- read.csv2("https://github.com/pjournal/mef03g-spo-R-ify/blob/master/SpotifyR/D.csv")
+    } else if (personaKey$key_name == "D#") {
+      personaInfo <- read.csv2("https://github.com/pjournal/mef03g-spo-R-ify/blob/master/SpotifyR/Dmj.csv")
+    } else if (personaKey$key_name == "E") {
+      personaInfo <- read.csv2("https://github.com/pjournal/mef03g-spo-R-ify/blob/master/SpotifyR/E.csv")
+    } else if (personaKey$key_name == "F") {
+      personaInfo <- read.csv2("https://github.com/pjournal/mef03g-spo-R-ify/blob/master/SpotifyR/F.csv")
+    } else if (personaKey$key_name == "F#") {
+      personaInfo <- read.csv2("https://github.com/pjournal/mef03g-spo-R-ify/blob/master/SpotifyR/Fmj.csv")
+    } else if (personaKey$key_name == "G") {
+      personaInfo <- read.csv2("https://github.com/pjournal/mef03g-spo-R-ify/blob/master/SpotifyR/G.csv")
+    } else if (personaKey$key_name == "G#") {
+      personaInfo <- read.csv2("https://github.com/pjournal/mef03g-spo-R-ify/blob/master/SpotifyR/Gmj.csv")
+    } else {
+      personaInfo <- read.csv2("https://github.com/pjournal/mef03g-spo-R-ify/blob/master/SpotifyR/Amj.csv")
+    }
+    as.data.frame(personaInfo)
+    
+  })
+}
+
+
+shinyApp(ui, server)
